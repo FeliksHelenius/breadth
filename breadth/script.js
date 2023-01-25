56f2316e30b1c66e91f0347d1e2ea8c7cdc9c4a4
FeliksHelenius <FeliksHelenius@gmail.com>
2023-01-25 17:07:20 +0100

    2 solutions

--- breadth/script.js
+++ breadth/script.js
@@ -1,31 +1,86 @@
 const root = document.querySelector('#root');
-let results = [];
-// 600px width 600px height
+const rootX = Math.round(root.getBoundingClientRect().x);
+const rootY = Math.round(root.getBoundingClientRect().y);
+const results = {
+  0: [],
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+};
+// not the actual number of obstacles
+const obstacles = 15;
+
 for (let i = 0; i < 36; i++) {
   let node = document.createElement('div');
-  node.className = 'node';
+  let random = Math.round(Math.random() * obstacles);
+  random == 0
+    ? ((node.className = 'obstacle'), (node.style.background = 'gray'))
+    : (node.className = 'node');
   root.appendChild(node);
 }
-
-//offsetLeft offsetTop
-let rootX = Math.round(root.getBoundingClientRect().x);
-let rootY = Math.round(root.getBoundingClientRect().y);
-//choose which Y section to check
-function checkX(y) {
-  y = y * 100;
-  for (i = 0; i < 6; i++) {
-    let x = i * 100;
-    let currentNode = document.elementFromPoint(rootX + x, rootY + y);
-    results.push(currentNode);
+function establishMap() {
+  function checkX(y) {
+    y = y * 100;
+    for (i = 0; i < 6; i++) {
+      let x = i * 100;
+      let currentNode = document.elementFromPoint(rootX + x, rootY + y);
+      // currentNode.textContent = x + y + currentNode.className;
+      results[y / 100].push(currentNode);
+    }
+  }
+  for (let i = 0; i < 6; i++) {
+    checkX(i);
   }
 }
-checkX(0);
+establishMap();
 
-console.log(results);
+function findPath(results, startX, startY) {
+  let stack = [[startY, startX]];
+  let visited = [[startY, startX]];
+  let path = [[startY, startX]];
+  let dx = [0, 0, 1, -1];
+  let dy = [1, -1, 0, 0];
 
+  while (stack.length > 0) {
+    let current = stack.pop();
+    let x = current[1];
+    let y = current[0];
+
+    for (let i = 0; i < 4; i++) {
+      let newX = x + dx[i];
+      let newY = y + dy[i];
+      if (
+        newY >= 0 &&
+        newY < 6 &&
+        newX >= 0 &&
+        newX < results[0].length &&
+        !visited.find((p) => p[0] == newY && p[1] == newX) &&
+        results[newY][newX].className != 'obstacle'
+      ) {
+        stack.push([newY, newX]);
+        visited.push([newY, newX]);
+        path.push([newY, newX]);
+      }
+    }
+  }
+  return path;
+}
+let answer = findPath(results, 0, 0);
+console.log(answer);
+count = 0;
+answer.forEach((element) => {
+  setTimeout(() => {
+    walk(element[0], element[1]);
+  }, 1000 + count * 200);
+  count++;
+});
+function walk(y, x) {
+  results[y][x].style.background = 'red';
+}
+//just to check what you're clicking on
 window.addEventListener('click', (e) => {
   console.log(e.clientX + 'x', e.clientY + 'y');
   console.log(document.elementFromPoint(e.clientX, e.clientY));
 });
-// console.log(test.getBoundingClientRect());
-// console.log(x + 'x');
--- weatherapi/script.js
+++ weatherapi/script.js
@@ -1,23 +1,110 @@
-const form = document.querySelector('.form');
-const button = document.querySelector('.submit');
-let answer = '';
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  answer = form.answer.value;
-  form.reset();
-});
+// create a 6x6 map
+let map = [];
+for (let i = 0; i < 6; i++) {
+  map[i] = [];
+  for (let j = 0; j < 6; j++) {
+    map[i][j] = 0;
+  }
+}
 
-console.log(answer);
+// place obstacles inside the map at random position
+let obstacleCount = 0;
+while (obstacleCount < 6) {
+  let x = Math.floor(Math.random() * 6);
+  let y = Math.floor(Math.random() * 6);
+  if (map[x][y] === 0) {
+    map[x][y] = 1;
+    obstacleCount++;
+  }
+}
 
-const apikey = 'qBU3EAEpRaj5Bn1ldCjDlsZH07pxRNgC';
-// ${locationKey}?apikey=qBU3EAEpRaj5Bn1ldCjDlsZH07pxRNgC&language=en-us&details=false&metric=true`;
-locationKey = '46';
-let url = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+// calculate the shortest path to traverse the whole map without touching the obstacles
+let path = [];
+let current = [0, 0];
+let next = [0, 0];
+let visited = [];
+for (let i = 0; i < 6; i++) {
+  visited[i] = [];
+  for (let j = 0; j < 6; j++) {
+    visited[i][j] = 0;
+  }
+}
 
-fetch(`${url}?apikey=${apikey}&q=stockholm`)
-  .then((response) => {
-    response.json();
-  })
-  .then((res) => {
-    console.log(res);
-  });
+let queue = [];
+queue.push(current);
+visited[current[0]][current[1]] = 1;
+
+while (queue.length > 0) {
+  current = queue.shift();
+  path.push(current);
+  if (current[0] === 5 && current[1] === 5) {
+    break;
+  }
+  // up
+  next = [current[0] - 1, current[1]];
+  if (
+    next[0] >= 0 &&
+    map[next[0]][next[1]] === 0 &&
+    visited[next[0]][next[1]] === 0
+  ) {
+    queue.push(next);
+    visited[next[0]][next[1]] = 1;
+  }
+  // down
+  next = [current[0] + 1, current[1]];
+  if (
+    next[0] < 6 &&
+    map[next[0]][next[1]] === 0 &&
+    visited[next[0]][next[1]] === 0
+  ) {
+    queue.push(next);
+    visited[next[0]][next[1]] = 1;
+  }
+  // left
+  next = [current[0], current[1] - 1];
+  if (
+    next[1] >= 0 &&
+    map[next[0]][next[1]] === 0 &&
+    visited[next[0]][next[1]] === 0
+  ) {
+    queue.push(next);
+    visited[next[0]][next[1]] = 1;
+  }
+  // right
+  next = [current[0], current[1] + 1];
+  if (
+    next[1] < 6 &&
+    map[next[0]][next[1]] === 0 &&
+    visited[next[0]][next[1]] === 0
+  ) {
+    queue.push(next);
+    visited[next[0]][next[1]] = 1;
+  }
+}
+
+// visually illustrate the map and draw the traversing path using html and css
+let mapDiv = document.getElementById('map');
+let pathDiv = document.getElementById('path');
+
+for (let i = 0; i < 6; i++) {
+  for (let j = 0; j < 6; j++) {
+    let div = document.createElement('div');
+    div.className = 'cell';
+    if (map[i][j] === 1) {
+      div.className += ' obstacle';
+    }
+    mapDiv.appendChild(div);
+  }
+}
+
+for (let i = 0; i < path.length; i++) {
+  let div = document.createElement('div');
+  div.className = 'path';
+  div.style.top = path[i][0] * 100 + 'px';
+  div.style.left = path[i][1] * 100 + 'px';
+  pathDiv.appendChild(div);
+
+  setTimeout(() => {
+    div.style.background = 'red';
+  }, 1000 + i * 100);
+}
